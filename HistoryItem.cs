75fa1ae050321d82cf22a8ac14491448d11ed475
5082300251-ui <5082300251@sjc.bz>
2025-11-24 23:24:08 -0600

    Add HistoryItem class for expression history

---
+++ HistoryItem.cs
@@ -0,0 +1,9 @@
+namespace XqueezeOS
+{
+    public class HistoryItem
+    {
+        public string Expr { get; set; }
+        public string Result { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
